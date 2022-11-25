021f86ff05d775558d44ea0b6669aa2caadf1050
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-25 19:28:14 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -6,7 +6,7 @@ import pandas as pd #manipulacao
 dados = pd.read_csv("diabetes.csv")
 
 
-#coleta os nomes das colunas
+'''#coleta os nomes das colunas
 nomes_colunas = dados.columns.to_list()
 tamanho = len(nomes_colunas)
 nomes_colunas = nomes_colunas[:tamanho-1]
@@ -17,7 +17,7 @@ features.shape,classes.shape
 #dividir entre treino e teste
 from sklearn.model_selection import train_test_split
 
-'''features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,classes,test_size=0.3,random_state=2)
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,classes,test_size=0.3,random_state=2)
 from sklearn.tree import DecisionTreeClassifier
 arvore = DecisionTreeClassifier()
 
