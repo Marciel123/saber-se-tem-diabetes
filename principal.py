2f5e5d3cf65b0fd853367000d31032cb0df76eb5
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-25 19:31:12 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -5,5 +5,11 @@ import pandas as pd #manipulacao
 ###################importando os dados do csv ########################
 dados = pd.read_csv("diabetes.csv")
 
-
+#coleta os nomes das colunas
+nomes_colunas = dados.columns.to_list()
+tamanho = len(nomes_colunas)
+nomes_colunas = nomes_colunas[:tamanho-1]
+features = dados[nomes_colunas]
+classes = dados['Outcome']
+features.shape,classes.shape
 
