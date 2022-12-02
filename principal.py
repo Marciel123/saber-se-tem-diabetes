566b363871d92ac1384adb2b3154f72f82a36363
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-12-02 18:36:52 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -38,6 +38,27 @@ print("resultado",resultado)
 #from sklearn import metrics
 
 #print(metrics.classification_report(classes_teste,resultado,target_names=['']))
+individuo = []
+diagnostico = ['Não diabético','Diabético']
+Pregnancies = int(input('Digite o número de gravidez do indivíduo'))
+individuo.append(Pregnancies)
+Glucose = int(input('Digite o número de glucose (0-199)'))
+individuo.append(Glucose)
+BloodPressure = int(input('Digite a pressão sanguínea (0-122)'))
+individuo.append(BloodPressure)
+SkinThickness = int(input('Digite a espessura da pele (0-99)'))
+individuo.append(SkinThickness)
+Insulin = int(input('Digite o valor da insulina (0-846)'))
+individuo.append(Insulin)
+BMI = float(input('Digite o IMC (0-67.1)'))
+individuo.append(BMI)
+#Diabetes Pedigree Function ou Função de linhagem de diabetes: indica a função que pontua a probabilidade de diabetes com base na história familiar
+DiabetesPedigreeFunction = float(input('Digite o pedigree de 0.07 a 2.2'))
+individuo.append(DiabetesPedigreeFunction)
+Age = int(input('Digite a idade (21-81)'))
+individuo.append(Age)
 
+resposta = arvore.predict([individuo])
+print(diagnostico[int(resposta)])
 
 
