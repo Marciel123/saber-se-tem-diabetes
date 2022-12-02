925e44c4a61879969b8fbff48547f4d3d39f347c
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-12-02 18:46:17 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd #manipulacao 
 
-
 ###################importando os dados do csv ########################
 dados = pd.read_csv("diabetes.csv")
 
@@ -14,7 +13,7 @@ classes = dados['Outcome']
 features.shape,classes.shape
 
 #dividir entre treino e teste
-from sklearn.model_selection import train_test_split
+'''from sklearn.model_selection import train_test_split
 
 features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,classes,test_size=0.3,random_state=2)
 from sklearn.tree import DecisionTreeClassifier
