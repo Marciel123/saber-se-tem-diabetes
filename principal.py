01e3d8ff922ab1f9587b967d57bb1617625f03a8
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-12-02 19:46:28 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -66,7 +66,7 @@ individuo.append(Age)
 resposta = arvore.predict([individuo])
 print(diagnostico[int(resposta)])
 
-if st.button(' diagnostico  '):
+if st.button('diagnostico'):
    resposta = arvore.predict([[Glucose,BloodPressure,SkinThickness,Insulin,DiabetesPedigreeFunction,Age]])#fara a predicao
   if resposta == 0:
     st.write('Diabetico!')
